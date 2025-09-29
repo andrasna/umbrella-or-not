b461fe9525b3c39325298490b1ab6f9be6e387f5
Andras Nagy <andrasnagy.contact@gmail.com>
2025-09-29 18:00:16 +0200

    Config tanstack query, add initial geocoding api fetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useQuery } from "@tanstack/react-query"
+
+async function getLocation() {
+  const res = await fetch("https://geocoding-api.open-meteo.com/v1/search?name=Budapest")
+  if (!res.ok) throw new Error("Failed request")
+  return res.json()
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { data, error, isLoading } = useQuery({
+    queryKey: ["getLocation"],
+    queryFn: getLocation,
+  })
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  console.log("data:", data)
+
+  if (isLoading) return <p>Loading…</p>
+  if (error) return <p>Error: {(error as Error).message}</p>
+
+  return <>Searching...</>
 }
 
 export default App
